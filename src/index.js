1285cbd09737dac72cf8b807ca5c29c17e8c2fc9
Payton Lower <paytown@me.com>
2019-05-22 15:58:52 -0700

    post working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
+import { ADD_POST } from './actions/actionCreators';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  title: null,
+  body: null
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case ADD_POST:
+      return { 
+        ...state,
+        title: action.payload.title,
+        body: action.payload.body
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: ADD_POST,
+  payload: {
+    title: 'My Cool Blog',
+    body: 'Hello world!'
+  }
+});
+console.log('added post', store.getState());
