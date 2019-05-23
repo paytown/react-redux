e88548e12445fbdb9cc2b1865a7d3b9eab0e6b26
Payton Lower <paytown@me.com>
2019-05-23 14:44:05 -0700

    baseline app hooked up

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-import { createStore } from 'redux';
-import { addPost, removePost, updatePost } from './actions/blogActions';
-import { addComment, removeComment } from './actions/commentActions';
-import reducers from './reducers/index';
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
 
-const store = createStore(reducers);
+render(
+  <App />,
+  document.getElementById('root')
+);
 
-store.dispatch(addPost('hi', 'body'));
-console.log('added post', store.getState());
 
-store.dispatch(addPost('hello', 'there'));
-console.log('added post2', store.getState());
+// import { createStore } from 'redux';
+// import { addPost, removePost, updatePost } from './actions/blogActions';
+// import { addComment, removeComment } from './actions/commentActions';
+// import reducers from './reducers/index';
 
-store.dispatch(addPost('post', 'three'));
-console.log('added post3', store.getState());
+// const store = createStore(reducers);
 
-store.dispatch(removePost(1));
-console.log('l8r', store.getState());
+// store.dispatch(addPost('hi', 'body'));
+// console.log('added post', store.getState());
 
-store.dispatch(updatePost(1, 'nah im post 2 now'));
-console.log('update', store.getState());
+// store.dispatch(addPost('hello', 'there'));
+// console.log('added post2', store.getState());
 
-store.dispatch(addComment(0, 'comment one'));
-console.log('comment1', store.getState());
+// store.dispatch(addPost('post', 'three'));
+// console.log('added post3', store.getState());
 
-store.dispatch(addComment(0, 'comment 2'));
-console.log('comment2', store.getState());
+// store.dispatch(removePost(1));
+// console.log('l8r', store.getState());
 
-store.dispatch(removeComment(0, 1));
-console.log('delete comment2', store.getState());
+// store.dispatch(updatePost(1, 'nah im post 2 now'));
+// console.log('update', store.getState());
+
+// store.dispatch(addComment(0, 'comment one'));
+// console.log('comment1', store.getState());
+
+// store.dispatch(addComment(0, 'comment 2'));
+// console.log('comment2', store.getState());
+
+// store.dispatch(removeComment(0, 1));
+// console.log('delete comment2', store.getState());
